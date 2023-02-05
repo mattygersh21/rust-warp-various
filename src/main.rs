ac1fea9739ce5cdcb9c663d5c4240e6d2e72326a
mattygersh21 <dakotaseia@hotmail.com>
2023-02-05 10:49:04 -0600

    * Removed hello world endpoint. * Refactored routes - split into APIs and static content sections. * Cleaned up dev.http.

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,22 @@ const WEB_FOLDER: &str = "web-folder/";
 
 #[tokio::main]
 async fn main() {
-    let hello_world = warp::path::end()
-        .and(warp::get())
-        .map(|| "Hello world from root!");
-    
+    // APIs
     let hi = warp::path("hi")
         .and(warp::get())
         .map(|| "Hello from  hi");
     
-    let content = warp::fs::dir(WEB_FOLDER);
+    let apis = hi;
     
-    let routes = hello_world
-        .or(hi)
-        .or(content);
+    // static content
+    let content = warp::fs::dir(WEB_FOLDER);
+    let root = warp::get()
+        .and(warp::path::end())
+        .and(warp::fs::file(format!("{}/index.html",WEB_FOLDER)));
+    let static_site = content.or(root);
+
+    let routes = apis
+        .or(static_site);
 
     println!("start web-server");
     warp::serve(routes).run(([127,0,0,1],8080)).await;
