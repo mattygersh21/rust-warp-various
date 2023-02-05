671d2400670e5f0331060f769697726e1ed2a0ca
mattygersh21 <dakotaseia@hotmail.com>
2023-02-05 09:15:46 -0600

    * Added API endpoint to /hi route. * Added routes variable to server multiple routes simulteanously on the same port.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,11 @@ use warp::Filter;
 #[tokio::main]
 async fn main() {
     let hello_world = warp::path::end().map(|| "Hello world from root!");
+    
+    let hi = warp::path("hi").map(|| "Hello from  hi");
+    
+    let routes = hello_world.or(hi);
 
     println!("start web-server");
-    warp::serve(hello_world).run(([127,0,0,1],8080)).await;
+    warp::serve(routes).run(([127,0,0,1],8080)).await;
 }
